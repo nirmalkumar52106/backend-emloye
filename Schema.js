43f2fa33685fbda10a4aeaaace45767b1839b4ff
nirmalkumar52106 <104349784+nirmalkumar52106@users.noreply.github.com>
2023-07-02 22:07:13 +0530

    first commit

--- Schema.js
+++ Schema.js
@@ -4,7 +4,8 @@ const SignupSchema = new mongoose.Schema({
     name: String,
     email: String,
     number:Number,
-    
+    img:String,
+
   }); 
   const SignupUser = mongoose.model('newemployes', SignupSchema);
 
